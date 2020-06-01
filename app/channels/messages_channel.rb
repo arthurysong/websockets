4044345448f454b6da76e014b67a9ac42e82db94
Arthur Song <arth3rs0ng@gmail.com>
2020-06-01 16:51:18 -0700

    Final changes

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,13 +1,20 @@
-class RoomsChannel < ApplicationCable::Channel
+class MessagesChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
-
     messages = Message.all
-    stream_for messages
-    RoomsChannel.broadcast_to(messages, messages)
+
+    stream_from "messages"
+    ActionCable.server.broadcast("messages", {type: "current_messages", messages: messages})
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  def create_message(data)
+    message = Message.create(content: data["content"])
+    
+    messages = Message.all
+    ActionCable.server.broadcast("messages", {type: "current_messages", messages: messages})
   end
 end
--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,87 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-    messages: []
+    newMessage: "",
+    messages: [],
+    socket: null
+  }
+
+  connectToActionCable = host => {
+    return (
+      new Promise ((resolve, error) => {
+        //create and connect
+        let socket = new WebSocket(host);
+
+        //handlers
+        socket.onopen = () => {resolve(socket)};
+        socket.onclose = () => {console.log('ws closed')};
+        socket.onerror = errors => {error(errors)};
+        socket.onmessage = event => {
+          let payload = JSON.parse(event.data);
+          
+          if (payload.message){
+            switch (payload.message.type) {
+              case 'current_messages':
+                  this.setState({
+                    messages: payload.message.messages
+                  })
+                  break;
+              default:
+                  break;
+            }
+          }
+        };
+      })
+    )
   }
 
   componentDidMount(){
-
+    this.connectToActionCable(`ws://localhost:3001/cable`)
+      .then(socket => {
+        const subscribe_info = {
+          command: 'subscribe',
+          identifier: JSON.stringify({channel: "MessagesChannel"})
+        }
+        // console.log('hi i connected, and now Im going to subscribe')
+        this.setState({
+          socket: socket
+        })
+        socket.send(JSON.stringify(subscribe_info));
+      });
   }
   
   renderMessages(){
-    this.state.messages.map((message, index) => <li key={index}>{message.content}</li>)
+    return (this.state.messages.map((message, index) => <li key={index}>{message.content}</li>))
+  }
+
+  submitHandler = event => {
+    event.preventDefault();
+    const newMessageInfo = {
+      command: "message",
+      identifier: JSON.stringify({channel: "MessagesChannel"}),
+      data: JSON.stringify({action: "create_message", content: this.state.newMessage })
+    }
+    this.state.socket.send(JSON.stringify(newMessageInfo))
+  }
+
+  changeHandler = event => {
+    this.setState({
+      newMessage: event.target.value
+    })
   }
 
   render() {
     return (
       <div className="App">
+        Messages Board:
         <ul>
           {this.renderMessages()}
         </ul>
+
+        <form onSubmit={this.submitHandler}>
+          <input type="text" onChange={this.changeHandler} value={this.state.newMessage} name="newMessage"/>
+          <input type="submit" value="Post"/>
+        </form>
       </div>
       );
     }
